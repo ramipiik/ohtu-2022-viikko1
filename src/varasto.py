0c7c680db337d9f7db95a34a40242f323d029aa2
Rami Piik <rami.piik@gmail.com>
2022-01-14 00:12:36 +0200

    pylint config and styling

--- src/varasto.py
+++ src/varasto.py
@@ -1,17 +1,9 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-        
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
+        self.tilavuus = max(tilavuus, 0)
+        if alku_saldo <= tilavuus:
             # mahtuu
-            self.saldo = alku_saldo
+            self.saldo = max(alku_saldo,0)
         else:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
@@ -31,15 +23,12 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
-
             return kaikki_mita_voidaan
-
         self.saldo = self.saldo - maara
-
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
